43aed3953bac06330504cf935ee9c9f1a81942ed
taqin <abdulmuttaqin456@gmail.com>
2024-11-13 16:51:39 +0700

    update App.tsx

---
+++ src/MainScreen.tsx
@@ -0,0 +1,10 @@
+import { View,Text } from "react-native";
+import { styles } from "./assets/style";
+
+export default function MainScreen() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Test</Text>
+        </View>
+    )
+}
---
+++ src/assets/style.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flexDirection: "column",
+        alignSelf: "center",
+    },
+    text : {
+        fontSize: 20,
+        fontWeight: "bold",
+        alignContent : "center",
+        marginTop: '100%'
+    }
+});
